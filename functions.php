c753c330b864f1b62b0523f361149f2f9299fc67
Matt Bernhardt <mjbernha@mit.edu>
2016-01-12 10:29:09 -0500

    Removes custom taxonomies, adds IE-specific styles to functions
    This closes #5
    
    Signed-off-by: Matt Bernhardt <mjbernha@mit.edu>

--- functions.php
+++ functions.php
@@ -7,23 +7,21 @@
  * @package Courtyard-blog
  */
 
-function taxonomy_init() {
-	// create a new taxonomy
-	register_taxonomy(
-		'department',
-		'post',
-		array(
-			'label' => __( 'Department' )
-		)
-	);
+/**
+ * Enqueue scripts.
+ */
+function courtyard_blog_enqueue_scripts() {
+	/**
+	 * IE Fallbacks.
+	 *
+	 * @link http://stackoverflow.com/questions/11564142/wordpress-enqueue-scripts-for-only-if-lt-ie-9
+	 */
+	wp_register_script( 'ie_html5shiv', get_bloginfo( 'stylesheet_url' ). '/js/html5.js', __FILE__, false, '3.7.2' );
+	wp_enqueue_script( 'ie_html5shiv' );
+	wp_script_add_data( 'ie_html5shiv', 'conditional', 'lt IE 9' );
 
-	// create a new taxonomy
-	register_taxonomy(
-		'meeting',
-		'post',
-		array(
-			'label' => __( 'Meeting' )
-		)
-	);
+	wp_register_script( 'ie_respond', '//cdn.jsdelivr.net/respond/1.4.2/respond.min.js', __FILE__, false, '1.4.2' );
+	wp_enqueue_script( 'ie_respond' );
+	wp_script_add_data( 'ie_respond', 'conditional', 'lt IE 9' );
 }
-add_action( 'init', 'taxonomy_init' );
+add_action( 'wp_enqueue_scripts', 'courtyard_blog_enqueue_scripts' );
