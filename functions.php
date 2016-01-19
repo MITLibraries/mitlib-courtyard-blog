288e73c1f0a03a7867129a957eb13278feef6a4e
Matt Bernhardt <mjbernha@mit.edu>
2016-01-19 10:58:04 -0500

    Reworks template logic to correspond with mockup
    Signed-off-by: Matt Bernhardt <mjbernha@mit.edu>

--- functions.php
+++ functions.php
@@ -7,23 +7,47 @@
  * @package Courtyard-blog
  */
 
-function taxonomy_init() {
-	// create a new taxonomy
-	register_taxonomy(
-		'department',
-		'post',
-		array(
-			'label' => __( 'Department' )
-		)
-	);
-
-	// create a new taxonomy
-	register_taxonomy(
-		'meeting',
-		'post',
-		array(
-			'label' => __( 'Meeting' )
-		)
+/**
+ * Posted_on rewrite
+ */
+function courtyard_posted_on() {
+	print '<span class="sep">' . esc_html__( 'Posted on ', 'twentyeleven' ) . '</span>';
+	printf( '<time class="entry-date" datetime="%1$s" pubdate>%2$s</time>',
+		esc_attr( get_the_date( 'c' ) ),
+		esc_html__( get_the_date(), 'twentyeleven' )
 	);
 }
-add_action( 'init', 'taxonomy_init' );
+
+/**
+ * Post title (with link)
+ */
+function courtyard_entry_link() {
+	printf( '<a href="%s" rel="bookmark">', esc_url( get_permalink() ) );
+	courtyard_entry_title();
+	print '</a>';
+}
+
+/**
+ * Post title rewrite
+ */
+function courtyard_entry_title() {
+	// Translators: used between list items, there is a space after the comma.
+	$category_single = get_the_category();
+	$category_single = $category_single[0]->name;
+
+	// Translators: used between list items, there is a space after the comma.
+	$tag_single = str_replace( 'sticky','',strip_tags( get_the_tag_list( '', '', '' ) ) );
+
+	if ( $tag_single ) {
+		$courtyard_post_title = __( '%1$s: %2$s Meeting', 'courtyard-blog' );
+	} elseif ( $category_single ) {
+		$courtyard_post_title = __( '%s$s Meeting', 'courtyard-blog' );
+	} else {
+		$courtyard_post_title = __( 'General Meeting', 'courtyard-blog' );
+	}
+	printf(
+		esc_html( $courtyard_post_title ),
+		esc_html( $category_single ),
+		esc_html( $tag_single )
+	);
+}
---
+++ template-parts/content.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Template part for displaying single posts.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Courtyard-blog
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<h2 class="entry-title"><?php courtyard_entry_link(); ?></h2>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php
+			the_content( sprintf(
+				/* translators: %s: Name of current post. */
+				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'courtyard' ), array( 'span' => array( 'class' => array() ) ) ),
+				the_title( '<span class="screen-reader-text">"', '"</span>', false )
+			) );
+		?>
+
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'courtyard' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php if ( 'post' === get_post_type() ) : ?>
+		<div class="entry-meta">
+			<?php courtyard_posted_on(); ?>
+		</div><!-- .entry-meta -->
+		<?php endif; ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
+
